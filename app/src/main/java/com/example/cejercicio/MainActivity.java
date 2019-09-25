beb7c6f8456acfd15070d49a9cf6acfed08c0930
christianraul <cristian_raul_2@hotmail.com>
2019-09-24 22:13:54 -0500

    Agregar la resta, multiplicación y división.

--- app/src/main/java/com/example/cejercicio/MainActivity.java
+++ app/src/main/java/com/example/cejercicio/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private EditText et01, et02;
-    private Button btncalcular;
+    private Button  btnsuma, btnresta, btnmultificar, btndividir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +22,61 @@ public class MainActivity extends AppCompatActivity {
 
         et01=findViewById(R.id.et1);
         et02=findViewById(R.id.et2);
-        btncalcular=findViewById(R.id.btnCalcular);
+        btnsuma=findViewById(R.id.btnSuma);
+        btnresta= findViewById(R.id.btnResta);
+        btnmultificar=findViewById(R.id.btnMultiplicar);
+        btndividir=findViewById(R.id.btnDivision);
 
 
-        btncalcular.setOnClickListener(new View.OnClickListener() {
+        btnsuma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int resultado = suma(Integer.valueOf(et01.getText().toString()),
                         Integer.valueOf(et02.getText().toString()));
 
-                Toast.makeText(MainActivity.this, "El resultado es: "+resultado, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "El resultado la suma es: "+resultado, Toast.LENGTH_SHORT).show();
 
             }
         });
+
+        btnresta.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int resultado= restar(Integer.valueOf(et01.getText().toString()),
+                                Integer.valueOf(et02.getText().toString()));
+                Toast.makeText(MainActivity.this, "El resultado de la resta es:"+resultado, Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnmultificar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int resultado= multi(Integer.valueOf(et01.getText().toString()),
+                        Integer.valueOf(et02.getText().toString()));
+                Toast.makeText(MainActivity.this, "El resultado de la Multiplicación es:"+resultado, Toast.LENGTH_SHORT).show();
+            }
+        });
+        btndividir.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int resultado= div(Integer.valueOf(et01.getText().toString()),
+                        Integer.valueOf(et02.getText().toString()));
+                Toast.makeText(MainActivity.this, "El resultado de la División es:"+resultado, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     public int suma(int num1, int num2){
         return num1+num2;
 
     }
-
+    public int restar(int num1, int num2){
+        return num1-num2;
+    }
+    public int multi(int num1, int num2){
+        return num1*num2;
+    }
+    public int div(int num1, int num2){
+        return num1/num2;
+    }
 }
